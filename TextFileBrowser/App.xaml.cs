d5571c42bfa4fc8e82693a3204a621bdd205f548
MAROO2KO <maros.adamkovic@gmail.com>
2023-01-29 13:48:21 +0100

    rls

--- TextFileBrowser/App.xaml.cs
+++ TextFileBrowser/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SearchTextFiles
+namespace TextFileBrowser
 {
     /// <summary>
     /// Interaction logic for App.xaml
